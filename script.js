596ca6a7b8fcec1fb10731c7fc53b4f456b20759
tan27 <tn2706@gmail.com>
2021-06-02 22:26:34 -0700

    Creation

---
+++ script.js
@@ -0,0 +1,19 @@
+
+var mybutton = document.getElementById('sticky');
+
+
+window.onscroll = function() {scrollFunction()};
+mybutton.addEventListener('click', topFunction)
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0; 
+}
