bbc5697cd598c9e1d8eee587d44297704a4946b4
tan27 <tn2706@gmail.com>
2021-06-03 19:49:21 -0700

    mobile

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 
-var mybutton = document.getElementById('sticky');
-
+var mybutton = document.querySelector('#sticky');
 
 window.onscroll = function() {scrollFunction()};
 mybutton.addEventListener('click', topFunction)
@@ -16,4 +15,24 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0; 
+}
+
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    // hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-links");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    // hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
 }
