5b20c37da18c66ed3a7c4f2cc247b8ee8813937b
tan27 <tn2706@gmail.com>
2021-06-05 16:13:48 -0700

    toggle bars

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ var sticky = document.getElementById("sticky");
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-links");
 const navLinks = document.querySelectorAll(".nav-links");
+const bars = document.querySelector(".fa-bars")
 
 window.onscroll = function() {scrollFunction()};
 sticky.addEventListener('click', topFunction)
 hamburger.addEventListener("click", mobileMenu);
 navLinks.forEach(n => n.addEventListener("click", closeMenu));
+bars.addEventListener('click', hamFunction);
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -24,6 +26,12 @@ function topFunction() {
 
 function mobileMenu() {
     navMenu.classList.toggle("active");
+    bars.classList.toggle("fa-times");
+
+}
+
+function hamFunction() {
+  bars.classList.toggle("fa-bars");
 }
 
 function closeMenu() {
